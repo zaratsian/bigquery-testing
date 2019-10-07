09ad7a80083e5f4b3911d2bb8c22219bb026c40a
zaratsian <dtz001@gmail.com>
2019-10-07 09:54:30 -0400

    Added deployment, service, and setup.sh

---
+++ setup.sh
@@ -0,0 +1,15 @@
+gcp_project_id="$(gcloud config get-value project)"
+
+gcloud builds submit --tag gcr.io/"$gcp_project_id"/gke-bq-testing ./main/.
+
+gcloud container clusters create bqtimetest-app \
+   --num-nodes 1 \
+   --enable-basic-auth \
+   --issue-client-certificate \
+   --zone us-east1
+
+sed -i 's/gcp_project_id/'"$gcp_project_id"'/' deployment.yaml
+
+kubectl apply -f deployment.yaml
+
+kubectl apply -f service.yaml
