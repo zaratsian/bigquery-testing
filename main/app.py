973fb5f1fcf92b3be72710a89774446ab2b70802
zaratsian <dtz001@gmail.com>
2019-10-08 08:01:37 -0400

    updated setup.sh

--- main/app.py
+++ main/app.py
@@ -8,11 +8,14 @@ app = Flask(__name__)
 
 @app.route('/query')
 def query():
-    start_time = datetime.datetime.now()
-    query_job = client.query(""" SELECT COUNT(*) FROM `bigquery-public-data.chicago_crime.crime` """)
-    runtime = (datetime.datetime.now() - start_time).seconds
-    results = [row for row in query_job.result()][0][0]
-    return jsonify({'results':results, 'runtime':'{} seconds'.format(runtime)})
+    try:
+        start_time = datetime.datetime.now()
+        query_job = client.query(""" SELECT COUNT(*) FROM `bigquery-public-data.chicago_crime.crime` """)
+        runtime = (datetime.datetime.now() - start_time).seconds
+        results = [row for row in query_job.result()][0][0]
+        return jsonify({'results':results, 'runtime':'{} seconds'.format(runtime)})
+    except Exception as e:
+        return jsonify({'results':'Unable to execute query. EXCEPTION: {}'.format(e), 'runtime':''})
 
 if __name__ == "__main__":
     app.run(debug=False,host='0.0.0.0',port=8080)
